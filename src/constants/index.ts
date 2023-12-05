e30a55791ab5a9d2fce1f17354b66d6dcd0936db
PALE13 <palezhou13@163.com>
2023-12-05 17:16:54 +0800

    完成了前端的构建，使用ant design pro快速构建，完成用户登录注册功能，管理员页面功能

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const SYSTEM_LOGO = "https://hbimg.huaban.com/d08d09907523ea5c3e67ca0db8d120514e0c25a62ca9-YMIZgW_fw658";
+
+/**
+ * 知识星球文档地址
+ */
+export const PLANET_LINK = "https://docs.qq.com/doc/DUG93dVNHbVZjZXpo";
